317a1da3733ea20a324673e695b56ef7383077dd
KauanGS2003 <129869187+KauanGS2003@users.noreply.github.com>
2023-09-26 06:39:13 -0300

    adicionando o arquivo conexao.php

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+    $servidor="localhost";
+    $usuario="root";
+    $senha="1234";
+    $db="Bonnadica";
+
+    $conexao=mysqli_connect($servidor, $usuario, $senha, $db);
+
+    if(!$conexao){
+        die("Houve um erro: ".mysqli_connect_errno());
+    }
+?>
