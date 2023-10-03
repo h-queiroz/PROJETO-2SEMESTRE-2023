c47e2f16885f21708f6629de5cd46f850fdfe3d8
KauanGS2003 <129869187+KauanGS2003@users.noreply.github.com>
2023-10-03 12:24:05 -0300

    Update cadastro.php
    melhorando o cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,17 +6,32 @@
     $email=$_POST['email'];
     $nome=$_POST['nome'];
     $senha=$_POST['senha'];
+    //verificando o email
+    $sql = "SELECT * from usuarios where email = '$email'";
+    $select = mysqli_query($conexao, $sql);
+    $result = mysqli_fetch_array($select);
 
-    //colocando o valor da variavel no banco de dados
-    $sql="INSERT INTO usuarios (email, nome, senha)
-        VALUES ('$email', '$nome', '$senha')";
-    //se der certo mostrar cadastrado feito com sucesso
-    if(mysqli_query($conexao, $sql)){
-        echo "<h1>cadastrado feito com sucesso</h1>";
+    //se existir outro email igual nao roda
+    if($result['email'] == $email){
+        echo"email ja existe ou esta vazio";
+        echo $email;
     }
-    //se der errado mostrar o erro
-    else{
-        echo "Error: ".$sql."<br>".mysqli_error($conexao);
-    }
+    //se não roda o codigo
+    else {
+        //criptografando a senha
+        $senha=password_hash($senha, PASSWORD_DEFAULT);
+        //criando o valor no banco de dados
+        $sql="INSERT INTO usuarios (email, nome, senha) VALUES ('$email', '$nome', '$senha')";
+        
+        
+        //se der certo mostrar cadastrado feito com sucesso
+         if(mysqli_query($conexao, $sql)){
+            echo "<h1>cadastrado feito com sucesso</h1>";
+        }
+        //se der errado mostrar o erro
+        else{
+            echo "Error: ".$sql."<br>".mysqli_error($conexao);
+        }
     mysqli_close($conexao);
+}
 ?>
