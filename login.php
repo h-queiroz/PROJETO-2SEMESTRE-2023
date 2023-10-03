bcafd0bc91c7908e85a5297b49146f9d0bf75dbe
KauanGS2003 <129869187+KauanGS2003@users.noreply.github.com>
2023-10-03 10:44:07 -0300

    Update login.php
    Update do login.php agora dando certo.

--- login.php
+++ login.php
@@ -1,28 +1,24 @@
 <?php
-	include("conexao.php");
+    include"conexao.php";
 
-if (isset($_POST['submit'])) {
+    $email=$_POST['email'];
+    $senha=$_POST['senha'];
 
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $sql="SELECT * FROM usuarios where email = '$email' and senha = '$senha'";
 
-    $sql = "SELECT id, email, senha FROM usuarios WHERE email = ?";
+    echo"$email<br>";
+    echo"$senha";
 
-    $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $stmt->bind_result($id, $email, $senha);
-    $stmt->fetch();
-    $stmt->close();
+    $verificar = mysqli_query($conexao, $sql);
+    echo"<pre>";
+    print_r($conexao);
+    print_r($verificar);
+    echo"</pre>";
 
-    if ($email === $email && password_verify($senha, $email)) {
-
-        $_SESSION['id'] = $id;
-        $_SESSION['email'] = $email;
-        header("painel.html"); 
-        exit();
-    } else {
-         echo "Nome de usuário ou senha incorretos.";
-    }
+   if (mysqli_num_rows($verificar)>0) {
+    echo "login com sucesso";
 }
+    else{
+        echo"deu erro";
+    }
 ?>
