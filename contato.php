c3cd395cf5de98b6a7140a25dc2050bb692bd5ef
KauanGS2003 <129869187+KauanGS2003@users.noreply.github.com>
2023-10-09 21:34:18 -0300

    Create contato.php

---
+++ contato.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="icon" href="imgs/pizza.png" type="image/x-icon">
+      <meta charset="UTF-8" />
+    <title>Bonna Dica</title>
+    <link rel="stylesheet" href="styles/contato.css" />
+  </head>
+  <body>
+    <a href="index.html">
+    <img src="imgs/pizza.png" alt="logo">
+  </a>
+
+    <div class="links-lado-direito">
+
+      <a href="cardapio.html">Cardapio</a>
+      <a href="contato.html" class="verde">Contato</a>
+      <a href="sobre.html">Sobre-Nós</a>
+      <a href="login.html">Login</a>
+      <a href="cadastro.html">Cadastro</a>
+    </div>
+<hr>
+  <div class="links-lado-esquerdo">
+    <a target="_blank" href="https://www.instagram.com/bonnadica/">instagram</a>
+    <a target="_blank" href="https://www.facebook.com/pizzariabonnadica/">facebook</a>
+   <a href="tel:+5511986024599">Clique aqui para ligar para (11) 99999-9999</a>
+
+    </div>
+  </body>
+</html>
