7629ad6105c89fdcb65547defbdbda2f8543153c
Tgrauth <133019541+Tgrauth@users.noreply.github.com>
2023-10-27 11:32:45 -0300

    crie carrinho.php e mudei pagina de contatos

---
+++ carrinho.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="icon" href="imgs/carrinho-img.jpg" type="image/x-icon">
+      <meta charset="UTF-8" />
+    <title>carrinho</title>
+    <link rel="stylesheet" href="styles/carrinho.css" />
+  </head>
+
+<body>
+  <a href="index.php">
+  <img src="imgs/pizza.png" alt="logo" class="logo">
+</a>
+  <div class="links-lado-direito">
+    <a href="cardapio.php">Cardapio</a>
+    <a href="contato.php" class="verde">Contato</a>
+    <a href="sobre.php">Sobre-Nós</a>
+    <a href="login.php">Login</a>
+    <a href="cadastro.php">Cadastro</a>
+
+  </div>  <!-- tag div pode ser o que voce quiser  -->
+
+<hr>
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
